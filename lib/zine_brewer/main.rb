3c93931ca6934cc00ce518edb4644ad857ffa760
市古明典 <akinori.ichigo@gmail.com>
2022-01-24 13:31:50 +0900

    ヘッダーで指定するOGP画像について、ファイル名として/([Ss](eries)?_/にマッチする場合はそのまま、マッチしない場合には記事IDを付けて出力するようにした。連載などで複数の記事通しでOGP画像を使う場合に対応。

--- lib/zine_brewer/main.rb
+++ lib/zine_brewer/main.rb
@@ -38,7 +38,7 @@ module ZineBrewer
                     end
       h = header.strip.split(/\n\n+/)
       @corner, @title, @lead, @author = [0, 1, 2, 4].map{|i| set_header_item(h[i], '')}
-      @pic = set_header_item(h[3], ''){ "#{@article_id}_#{h[3]}" }
+      @pic = set_header_item(h[3], ''){ /^[Ss](eries)?_/ =~ h[3] ? h[3] : "#{@article_id}_#{h[3]}" }
       @css = set_header_item(h[5], ''){ h[5].each_line.map{|i| '.c-article_content ' + i }.join }
 
       @pp_header = make_pp_header
