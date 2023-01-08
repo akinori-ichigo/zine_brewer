61622a72eb21f9e1052eb2c8377b8073e3626cd6
市古明典 <akinori.ichigo@gmail.com>
2023-01-09 00:50:26 +0900

    新機能追加に合わせて、テストを書き換えた。

--- spec/zine_brewer_spec.rb
+++ spec/zine_brewer_spec.rb
@@ -7,30 +7,30 @@ RSpec.describe ZineBrewer do
   it "has a version number" do
     expect(ZineBrewer::VERSION).not_to be nil
   end
+
+  context "Test 01" do
+    ZineBrewer::Application.new("spec/testfiles/01/01_whole.txt").write_out
+    it "has a document with whole elements" do
+      expect(File.open("spec/testfiles/01/proof/body.txt").read.strip).to \
+        eq File.open("spec/testfiles/01/01_body.txt").read.strip
+    end
   
-  it "has a document with whole elements" do
-    expect(ZineBrewer::Application.new("spec/testfiles/01_whole.txt").pretty_print.strip).to \
-      eq File.open("spec/testfiles/01_whole.html").read.strip
+    it "puts the header file" do
+      expect(File.open("spec/testfiles/01/proof/header.txt").read.strip).to \
+        eq File.open("spec/testfiles/01/01_header.txt").read.strip
+    end
   end
 
-  it "puts the header file" do
-    expect(ZineBrewer::Application.new("spec/testfiles/01_whole.txt").make_pp_header.strip).to \
-      eq File.open("spec/testfiles/01_header.txt").read.strip
+  context "Test 02" do
+    ZineBrewer::Application.new("spec/testfiles/02/02_definition_table_th_width.txt").write_out
+    it "has a document with th width of the definition table" do
+      expect(File.open("spec/testfiles/02/proof/body.txt").read.strip).to \
+        eq File.open("spec/testfiles/02/02_body.txt").read.strip
+    end
+  
+    it "puts the header file" do
+      expect(File.open("spec/testfiles/02/proof/header.txt").read.strip).to \
+        eq File.open("spec/testfiles/02/02_header.txt").read.strip
+    end
   end
-
-  it "has the width attribute of <th> element at 1st column" do
-    expect(ZineBrewer::Application.new("spec/testfiles/02_definition_table_th_width.txt").pretty_print.strip).to \
-      eq File.open("spec/testfiles/02_definition_table_th_width.html").read.strip
-  end
-
-  it "puts the header file again" do
-    expect(ZineBrewer::Application.new("spec/testfiles/03_whole.txt").make_pp_header.strip).to \
-      eq File.open("spec/testfiles/03_header.txt").read.strip
-  end
-
-  it "has the book introduction column correctly" do
-    expect(ZineBrewer::Application.new("spec/testfiles/04_whole.txt").pretty_print.strip).to \
-      eq File.open("spec/testfiles/04_book_column.html").read.strip
-  end
-
 end
