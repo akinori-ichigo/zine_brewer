28d6049e55f922056a73059ae3faabcd1afb37a7
Akinori-Ichigo <akinori.ichigo@gmail.com>
2025-06-21 14:36:30 +0900

    テストを仕様変更に合わせてアップデートした。

--- spec/zine_brewer_spec.rb
+++ spec/zine_brewer_spec.rb
@@ -37,4 +37,35 @@ RSpec.describe ZineBrewer do
         eq File.open("spec/testfiles/02/02_header.txt").read.strip
     end
   end
+
+  context "Test 03" do
+    brewer = ZineBrewer.document.new
+    brewer.convert("spec/testfiles/03/03_whole.txt")
+    brewer.write_out
+    it "has a document with whole elements" do
+      expect(File.open("spec/testfiles/03/proof/body.txt").read.strip).to \
+        eq File.open("spec/testfiles/03/03_body.txt").read.strip
+    end
+  
+    it "puts the header file" do
+      expect(File.open("spec/testfiles/03/proof/header.txt").read.strip).to \
+        eq File.open("spec/testfiles/03/03_header.txt").read.strip
+    end
+  end
+
+  context "Test 04" do
+    brewer = ZineBrewer.document.new
+    brewer.convert("spec/testfiles/04/04_whole.txt")
+    brewer.write_out
+    it "has a document with whole elements" do
+      expect(File.open("spec/testfiles/04/proof/body.txt").read.strip).to \
+        eq File.open("spec/testfiles/04/04_body.txt").read.strip
+    end
+  
+    it "puts the header file" do
+      expect(File.open("spec/testfiles/04/proof/header.txt").read.strip).to \
+        eq File.open("spec/testfiles/04/04_header.txt").read.strip
+    end
+  end
+
 end
